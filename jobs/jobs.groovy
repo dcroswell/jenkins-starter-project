852b55cfdf94cf9a479d1eddc78d4cac54871ba3
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-04 17:03:22 +1000

    Another commit

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -1,4 +1,7 @@
-    scm {
+def signature = 'new groovy.json.JsonSlurperClassic'
+org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get().approveSignature(signature)
+
+scm {
         github('dcroswell/jenkins-starter-project', 'master')
     }
     triggers {
