163a3c3c3aa700f687403819b3fd7fa370be3b31
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-05 11:21:53 +1000

    another change

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -4,5 +4,5 @@ import utilities.MyUtilities
 def myJob = job('example')
 MyUtilities.addMyFeature(myJob)
 
-def out = new Output()
+def out = new MyUtilities.Output()
 out.info("SOME VERY USEFUL INFORMATION")
