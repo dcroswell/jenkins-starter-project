d75aa57bece5e19910d4a46cb700b0b6fd071830
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-05 11:19:12 +1000

    Output

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -3,3 +3,6 @@ import utilities.MyUtilities
 
 def myJob = job('example')
 MyUtilities.addMyFeature(myJob)
+
+def out = new Output()
+out.info("SOME VERY USEFUL INFORMATION")
--- jobs/utilities/MyUtilities.groovy
+++ jobs/utilities/MyUtilities.groovy
@@ -1,9 +1,20 @@
 package utilities
 
+import java.util.logging.Logger
+
 class MyUtilities {
     static void addMyFeature(def job) {
         job.with {
             description('Arbitrary feature')
         }
+    }
+}
+
+class Output {
+    private static final Logger LOGGER = Logger.getLogger(Output.class.getName());
+
+    def info(msg){
+        LOGGER.info("${msg}")
+        echo "[INFO] ${msg}" <-- gives me an exception described below
     }
 }
