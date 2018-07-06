dd6b343e5f752b77a044260affaecd185d5cdcc5
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-06 11:06:51 +1000

    Hello World

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -4,9 +4,11 @@ import utilities.MyUtilities
 def myJob = job('example')
 MyUtilities.addMyFeature(myJob)
 
+System.out.println("Hello World from Don")
+
 def out
 def config = new HashMap()
 def bindings = getBinding()
 config.putAll(bindings.getVariables())
 out = config['out']
-out.println "Printed do Jenkins console."
+out.println "Printed to Jenkins console."
