1c0a2dc1e18ba95de7a03b9f50bf425167a14544
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-09 16:18:15 +1000

    More fucking around

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -1,18 +1,15 @@
 /**
  * Created by Don Croswell on 9/07/2018.
- * Build the script with: groovyc ./jobs/jobs.groovy
  */
- 
-package jobs
 
 import jenkins.automation.builders.*
-import jobs.Utilities
-
-def utils = new jobs.Utilities()
+File utilitiesFile = new File("./jobs/Utilities.groovy");
+Class utilitiesClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(utilitiesFile);
+GroovyObject utils = (GroovyObject) utilitiesClass.newInstance();
 
 def myJob = job('example')
-Utilities.addMyFeature(myJob)
+utils.addMyFeature(myJob)
 
 
-def message = "Hello World from Don!"
-Utilities.loggit(message)
+// def message = "Hello World from Don!"
+// utils.loggit(message)
