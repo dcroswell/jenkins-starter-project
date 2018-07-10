8a74d27fb71375bf05b8756320dbd492fe6cb8ae
Donald Croswell <donald.croswell@williamhill.com.au>
2018-07-10 13:45:34 +1000

    Gotta love the warmth though!

--- jobs/jobs.groovy
+++ jobs/jobs.groovy
@@ -18,6 +18,16 @@ GroovyObject utils = (GroovyObject) utilitiesClass.newInstance();
 // def message = "Hello World from Don in ${env}"
 // println message
 
-def pipe = job.builders.createPipeline("Dev Jenkins Pipeline")
+// This shows a simple build wrapper example, using the AnsiColor plugin.
+node {
+    // This displays colors using the 'xterm' ansi color map.
+    ansiColor('xterm') {
+        // Just some echoes to show the ANSI color.
+        stage "\u001B[31mI'm Red\u001B[0m Now not"
+    }
+}
+
+
+def pipe = job("Dev Jenkins Pipeline")
 //def pipe = job("Dev Jenkins Pipeline")
 utils.createPipeline(pipe)
